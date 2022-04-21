14cdf0c54b8d2374d5c8d355d97c896f5f671077
Stephen <stephen.stengel@cwu.edu>
2022-04-21 15:03:37 -0700

    Added saving the datasets. It's quite large on disc though. Also added splitting off a test set.

--- load-dataset.py
+++ load-dataset.py
@@ -22,10 +22,15 @@ import tensorflow as tf
 DATASET_DIRECTORY = "../aminals/ftp.wsdot.wa.gov/public/I90Snoq/Biology/thermal/614s/"
 INTERESTING_DIRECTORY = "../aminals/ftp.wsdot.wa.gov/public/I90Snoq/Biology/thermal/614s/interesting/"
 NOT_INTERESTING_DIRECTORY = "../aminals/ftp.wsdot.wa.gov/public/I90Snoq/Biology/thermal/614s/not interesting/"
-COMPILED_FILE_DIRECTORY = "./dataset/"
 DATASET_COPY_FOLDER = "./tmpdata/"
 DATASET_COPY_FOLDER_INT = "./tmpdata/int/"
 DATASET_COPY_FOLDER_NOT = "./tmpdata/not/"
+
+DATASET_SAVE_DIR = "./dataset/"
+TRAIN_SAVE_DIRECTORY = "./dataset/train/"
+VAL_SAVE_DIRECTORY = "./dataset/val/"
+TEST_SAVE_DIRECTORY = "./dataset/test/"
+
 
 
 CLASS_INTERESTING = 0
@@ -39,12 +44,8 @@ def main(args):
 	checkArgs(args)
 	print("DATASET_DIRECTORY: " + str(DATASET_DIRECTORY))
 	
-	if not os.path.isdir(DATASET_COPY_FOLDER):
-		os.mkdir(DATASET_COPY_FOLDER)
-	if not os.path.isdir(DATASET_COPY_FOLDER_INT):
-		os.mkdir(DATASET_COPY_FOLDER_INT)
-	if not os.path.isdir(DATASET_COPY_FOLDER_NOT):
-		os.mkdir(DATASET_COPY_FOLDER_NOT)
+	makeDirectories()
+
 	
 	print("Creating file structure...")
 	createFileStructure(INTERESTING_DIRECTORY, DATASET_COPY_FOLDER_INT)
@@ -58,18 +59,55 @@ def main(args):
 	#DATASET_COPY_FOLDER
 	
 	#These could change later
-	# ~ img_height = 180
-	# ~ img_width = 180
-	img_height = 600
-	img_width = 800
+	img_height = 100
+	img_width = 100
+	# ~ img_height = 600
+	# ~ img_width = 800
 	batch_size = 32
 
+	print("creating the datasets...")
+	train_ds, val_ds, test_ds = createAnimalsDataset(DATASET_COPY_FOLDER, img_height, img_width, batch_size)
+	print("Done!")
+	
+	print("Saving datasets...")
+	tf.data.experimental.save(train_ds, TRAIN_SAVE_DIRECTORY)
+	tf.data.experimental.save(val_ds, VAL_SAVE_DIRECTORY)
+	tf.data.experimental.save(test_ds, TEST_SAVE_DIRECTORY)
+	print("Done!")
 
-	train_ds, val_ds = createAnimalsDataset(DATASET_COPY_FOLDER, img_height, img_width, batch_size)
 	
 
 	return 0
 
+
+
+#There is an easier way.
+def makeDirectories():
+	if not os.path.isdir(DATASET_COPY_FOLDER):
+		os.mkdir(DATASET_COPY_FOLDER)
+	if not os.path.isdir(DATASET_COPY_FOLDER_INT):
+		os.mkdir(DATASET_COPY_FOLDER_INT)
+	if not os.path.isdir(DATASET_COPY_FOLDER_NOT):
+		os.mkdir(DATASET_COPY_FOLDER_NOT)
+	
+	if not os.path.isdir(DATASET_SAVE_DIR):
+		os.mkdir(DATASET_SAVE_DIR)
+	if not os.path.isdir(TRAIN_SAVE_DIRECTORY):
+		os.mkdir(TRAIN_SAVE_DIRECTORY)
+	if not os.path.isdir(VAL_SAVE_DIRECTORY):
+		os.mkdir(VAL_SAVE_DIRECTORY)
+	if not os.path.isdir(TEST_SAVE_DIRECTORY):
+		os.mkdir(TEST_SAVE_DIRECTORY)
+
+
+#Takes some images from the validation set and sets the aside for the test set.
+def createTestSet(val_ds):
+	val_batches = tf.data.experimental.cardinality(val_ds)
+	test_dataset = val_ds.take(val_batches // 5)
+	val_ds = val_ds.skip(val_batches // 5)
+	
+	return val_ds, test_dataset
+	
 
 
 #Must use tf.keras.layers.Rescaling(1./255) as first layer in model !!!
@@ -113,8 +151,10 @@ def createAnimalsDataset(baseDirectory, img_height, img_width, batch_size):
 	normalized_train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 	normalized_val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
 
+	normalized_val_ds, normalized_test_ds = createTestSet(normalized_val_ds)
 
-	return normalized_train_ds, normalized_val_ds
+	return normalized_train_ds, normalized_val_ds, normalized_test_ds
+
 
 def createFileStructure(baseDirSource, destination):
 	copyDatasetToTMP(baseDirSource, destination)
