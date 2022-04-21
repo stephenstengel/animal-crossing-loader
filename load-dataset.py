2cae9626b71615b2c7ebf08636546a147710d4b1
Stephen <stephen.stengel@cwu.edu>
2022-04-21 09:12:37 -0700

    Initial commit.

---
+++ load-dataset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  load-dataset.py
+#  
+#  2022 Stephen Stengel <stephen.stengel@cwu.edu>
+#  
+#  Script to load the dataset into file form for easy importing.
+#  
+
+
+def main(args):
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
