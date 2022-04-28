28189950990846271df5b2c81dbb4b51d386b9b3
Stephen <stephen.stengel@cwu.edu>
2022-04-27 23:24:12 -0700

    CHanged picture input to grayscale. Fixed printing error.

--- load-dataset.py
+++ load-dataset.py
@@ -147,7 +147,7 @@ def saveDatasets(train_ds, trainDir, val_ds, valDir, test_ds, testDir):
 def createAnimalsDataset(baseDirectory, img_height, img_width, batch_size):
 	train_ds = tf.keras.preprocessing.image_dataset_from_directory(
 		baseDirectory,
-		color_mode = "rgb",
+		color_mode = "grayscale",
 		validation_split=0.2,
 		subset="training",
 		seed=123,
@@ -156,7 +156,7 @@ def createAnimalsDataset(baseDirectory, img_height, img_width, batch_size):
 
 	val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 		baseDirectory,
-		color_mode = "rgb",
+		color_mode = "grayscale",
 		validation_split=0.2,
 		subset="validation",
 		seed=123,
@@ -164,17 +164,17 @@ def createAnimalsDataset(baseDirectory, img_height, img_width, batch_size):
 		batch_size=batch_size)
 
 	if TEST_PRINTING:
+		class_names = train_ds.class_names
+		print("class names: " + str(class_names))
+
 		plt.figure(figsize=(10, 10))
 		for images, labels in train_ds.take(1):
 			for i in range(9):
 				ax = plt.subplot(3, 3, i + 1)
-				plt.imshow(images[i].numpy().astype("uint8"))
+				plt.imshow( images[i] , cmap='gray')
 				plt.title(class_names[labels[i]])
 				plt.axis("off")
 			plt.show()
-
-	class_names = train_ds.class_names
-	print("class names: " + str(class_names))
 
 	# ~ normalization_layer = tf.keras.layers.Rescaling(1./255) #for new versions
 	normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255) #for old versions
