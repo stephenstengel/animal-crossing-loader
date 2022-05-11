16d85eb61f854cbc47a475edc7299b3672c5ada0
AlexWorland <59108991+AlexWorland@users.noreply.github.com>
2022-05-10 17:14:59 -0700

    fixed issue with missing docker parameter

--- runWithDockerContainer_WINDOWS.bat
+++ runWithDockerContainer_WINDOWS.bat
@@ -1,2 +1,2 @@
 docker build -t "animal-crossing-loader" .
-docker run --rm -v %cd%:/app/ -w /app/ animal-crossing-loader python3 load-dataset.py -d
+docker run --gpus all --rm -v %cd%:/app/ -w /app/ animal-crossing-loader python3 load-dataset.py -d
